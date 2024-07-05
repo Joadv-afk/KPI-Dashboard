54d38899efe58b36069a032c4b2897e7e10cd53e
Joadv21 <jordyrafif21@gmail.com>
2024-07-05 16:03:20 +0700

    Reconstruct

--- scripts.js
+++
@@ -1,6 +0,0 @@
-function showReport() {
-    const mainContent = document.querySelector('#main-content');
-    mainContent.innerHTML = `
-        <iframe title="Dashboard Report" width="1024" height="1060" src="https://app.powerbi.com/view?r=eyJrIjoiMmMwZGJhMjItMGQ5My00YTNjLWE1ZWQtY2UxN2U5YWY5NTBiIiwidCI6IjkwYWZmZTBmLWMyYTMtNDEwOC1iYjk4LTZjZWI0ZTk0ZWYxNSIsImMiOjEwfQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe>
-    `;
-}
